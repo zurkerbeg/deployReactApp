48fcb0039d4eb5308476f1afd91bfef85f2b5c0a
zurkerbeg <zurkerbeg@gmail.com>
2020-06-29 21:36:40 +0700

    Updates

---
+++ precache-manifest.f92885c8f7e1a99ea5411de28bcbd41a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "490abb556aa5208eb568b7fb6a9d8ea5",
+    "url": "/deployReactApp/index.html"
+  },
+  {
+    "revision": "7d72f8396777a330db21",
+    "url": "/deployReactApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cb43c5dd6ae9c5788a7b",
+    "url": "/deployReactApp/static/js/2.da3882de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deployReactApp/static/js/2.da3882de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d72f8396777a330db21",
+    "url": "/deployReactApp/static/js/main.7d7920ab.chunk.js"
+  },
+  {
+    "revision": "152d1a6398e85bdd7f76",
+    "url": "/deployReactApp/static/js/runtime-main.3479bf39.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deployReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
